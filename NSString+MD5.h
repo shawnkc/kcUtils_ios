a572c3b80e00e099abdb3da71f8d240ba5d8e74b
Shawn Casey <shawn.casey@gmail.com>
2013-11-26 17:04:26 -0600

    - simple MD5 impl

---
+++ NSString+MD5.h
@@ -0,0 +1,16 @@
+//
+//  NSString+MD5.h
+//  YvolvrSDK
+//
+//  Created by Shawn Casey on 11/26/13.
+//  Copyright (c) 2013 Yvolvr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+
+- (NSString *)sha1;
+- (NSString *)md5;
+
+@end
---
+++ NSString+MD5.m
@@ -0,0 +1,43 @@
+//
+//  NSString+MD5.m
+//
+//  Created by Shawn Casey on 11/26/13.
+//
+
+#import "NSString+MD5.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSString (MD5)
+
+- (NSString *)sha1 {
+    const char *cstr = [self UTF8String];
+    NSData *data = [NSData dataWithBytes:cstr length:self.length];
+    
+    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+    
+    CC_SHA1(data.bytes, (CC_LONG)data.length, digest);
+    
+    NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) {
+        [output appendFormat:@"%02x", digest[i]];
+    }
+    
+    return output;
+    
+}
+
+- (NSString *)md5 {
+    const char *cStr = [self UTF8String];
+    unsigned char digest[16];
+    CC_MD5( cStr, (CC_LONG)strlen(cStr), digest ); // This is the md5 call
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [output appendFormat:@"%02x", digest[i]];
+    }
+    return  output;
+}
+
+@end
